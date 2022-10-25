da2a562be5e4a1e7133bf93bc88ea87d63d58196
Jimmy <70441896+Chromechef@users.noreply.github.com>
2022-10-25 09:16:13 -0400

    Update to fix bug in hamburger menu

--- index.js
+++ index.js
@@ -1,62 +1,47 @@
-const hiddenMenu = document.getElementById("hidden-menu")
-const hamburgerCheckbox = document.getElementById("hamburger-checkbox")
+const hiddenMenu = document.getElementById("hidden-menu");
+const hamburgerCheckbox = document.getElementById("hamburger-checkbox");
 
-const hiddenElements = document.querySelectorAll(".delay")
+const hiddenElements = document.querySelectorAll(".delay");
 
-let checked = false
+hamburgerCheckbox.addEventListener("change", e => {
+  hiddenMenu.classList.toggle("display-none");
+});
 
-hamburgerCheckbox.addEventListener("click", () => {
-
-    if (checked === false) {
-        hiddenMenu.classList.remove("display-none")
-        checked = true
-    } else if (checked) {
-        hiddenMenu.classList.add("display-none")
-        checked = false
-
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
+  });
+});
 
-})
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
-    })
-})
-
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach(el => observer.observe(el));
 
 function sendEmail() {
-    Email.send({
-        SecureToken: "04900a6f-4f14-42bd-8efc-df63a1555af5",
-        To: 'jimmy@webtotabledesigns.com',
-        From: "chefjimmy09@gmail.com",
-        Subject: "Email from Web to table Designs",
-        Body: "Name : " + document.getElementById("name").value
-            + "<br> Email: " + document.getElementById("email").value
-            + "<br> Phone Number: " + document.getElementById("phone").value
-            + "<br> Message: " + document.getElementById("message").value
-    }).then(
-        message => alert("mail has been sent sucessfully")
-    )
-
+  Email.send({
+    SecureToken: "04900a6f-4f14-42bd-8efc-df63a1555af5",
+    To: "jimmy@webtotabledesigns.com",
+    From: "chefjimmy09@gmail.com",
+    Subject: "Email from Web to table Designs",
+    Body:
+      "Name : " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Phone Number: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then(message => alert("mail has been sent sucessfully"));
 }
 
-// let inputBox = document.getElementById("hamburger-checkbox")
-
-// inputBox.addEventListener("click", function () {
-//     inputBox.checked = true
-// })
-
-// let check = document.querySelectorAll(".close")
-// check.forEach(uncheck => uncheck.addEventListener("click",
-//     function () {
-//         inputBox.checked = false
-//         checked = false
-//     }))
+hiddenMenu.addEventListener("click", e => {
+  if (e.target.tagName === "A") {
+    hiddenMenu.classList.toggle("display-none");
+    hamburgerCheckbox.checked = false;
+  }
+});
 
 
